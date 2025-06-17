831d3e93378f7ee8292ea37b9a992c02cf315df8
[vlnchieff] <[juschieff@gmail.com]>
2025-06-17 14:50:32 -0400

    week 2 update

---
+++ week-2/app.py
@@ -0,0 +1,20 @@
+"""
+Author: Chris Weaver
+Date: 2025-06-17
+File Name:
+Description: A simple Python script that greets the user by name.
+""  """
+
+#age = 25
+#first_name = "Alice"
+#music = "Jazz"
+#print(music)
+
+
+#name = input('What is your name? ')
+#print("Hello, " + name + "!")
+
+# Here is the Homework
+
+print('"Nobody can give you freedom. Nobody can give you equality or justice or anything. If you are a man, you take it."')
+print("- Malcom X")
