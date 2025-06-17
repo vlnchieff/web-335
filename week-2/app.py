bfb1386d9cfc28a08d4a96f3d6ee2395a9edae6d
[vlnchieff] <[juschieff@gmail.com]>
2025-06-17 18:29:13 -0400

    homework update

--- week-2/app.py
+++ week-2/app.py
@@ -1,21 +1,24 @@
 """
 Author: Chris Weaver
 Date: 2025-06-17
-File Name:
+File Name: app.py
 Description: A simple Python script that greets the user by name.
-""  """
+"""
 
-#age = 25
-#first_name = "Alice"
-#music = "Jazz"
-#print(music)
+"""
+age = 25
+first_name = "Alice"
+music = "Jazz"
+print(first_name)
 
-
-#name = input('What is your name? ')
-#print("Hello, " + name + "!")
+name = input('What is your name? ')
+print("Hello, " + name + "!")
+"""
 
 # Here is the Homework
-print("Hello, world!")
+
+
+print("Hello, world!") #Print a greeting message
 print("Here's the quote of the day:")
-print('"Nobody can give you freedom. Nobody can give you equality or justice or anything. If you are a man, you take it."')
-print("- Malcom X")
+print('"Nobody can give you freedom. Nobody can give you equality or justice or anything. If you are a man, you take it."') #Print a quote
+print("- Malcolm X") #Print the author of the quote
